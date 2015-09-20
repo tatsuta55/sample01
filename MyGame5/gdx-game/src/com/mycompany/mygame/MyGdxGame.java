94a822a98167c2189918853cffaa24ab638865dc
tatsuta <tofor80g@gmail.com>
2015-09-20 22:09:20 +0900

    test

--- MyGame5/gdx-game/src/com/mycompany/mygame/MyGdxGame.java
+++ MyGame5/gdx-game/src/com/mycompany/mygame/MyGdxGame.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -11,6 +13,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 
 
@@ -69,6 +73,34 @@ public class MyGdxGame implements ApplicationListener
 	@Override
 	public void render()
 	{
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		mCurrentDeltaTime += deltaTime;
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+
+		TextureRegion keyframe = anim.getKeyFrame(mCurrentDeltaTime);
+		batch.draw(keyframe, 0, 0, SIZE_CHR*2, SIZE_CHR*2);
+
+		if(Gdx.input.justTouched()){
+			touchCount++;
+			coin.play();
+			int x = Gdx.input.getX();
+			int y = Gdx.input.getY();
+			Vector3 coords = camera.unproject(new Vector3(x,y,0));
+			Gdx.app.log("MyGdxGame",String.format("(x,y)=(%d,%d), coords(x,y)=(%f,%f)", x,y,coords.x,coords.y));
+
+		}
+
+		final String text = (touchCount <= 0)?"Are you Ready":"Of course.\ncount=" + touchCount;
+		glyph.setText(font, text,Color.WHITE,0,Align.center, true);
+		font.draw(batch,glyph,VIEWPORT_WIDTH*0.5f,VIEWPORT_HEIGHT*0.5f+glyph.height*0.5f);
+
+		batch.end();
 
 	}
 
